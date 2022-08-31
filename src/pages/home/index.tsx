d6102ad8360dc8fb04c3ad2a7d189839a318c127
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2022-08-30 22:30:12 -0300

    feat: coffee card

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,45 +2,181 @@ import { ShoppingCart, Package, Timer, Coffee } from 'phosphor-react';
 import bigCoffeeImg from '../../assets/big-coffee.svg';
 import backgroundImg from '../../assets/background.svg';
 
-import { Benefits, BenefitsContainer, FlashyMessageContainer, FlashyTitle, InformationalSpace, PresentationCardContainer, SubTitle } from './styles';
+import {
+  Benefits,
+  BenefitsContainer,
+  FlashyMessageContainer,
+  FlashyTitle,
+  InformationalSpace,
+  PresentationCardContainer,
+  SubTitle,
+  CoffeeListContainer,
+  CoffeeListTitle,
+  CoffeeList,
+  CoffeeCard
+} from './styles';
 
 export function Home() {
   return (
-    <PresentationCardContainer style={{
-      backgroundImage: `url(${backgroundImg})`
-    }}>
-      <InformationalSpace>
-        <FlashyMessageContainer>
-          <FlashyTitle>Encontre o café perfeito para qualquer hora do dia</FlashyTitle>
-          <SubTitle>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</SubTitle>
-        </FlashyMessageContainer>
-      
-        <BenefitsContainer>
-          <Benefits>
-            <span><ShoppingCart size={20} /></span>  
-            <span>Compra simples e segura</span>
-          </Benefits>
-          <Benefits>
-            <span><Package size={20} /></span>
-            <span>Embalagem mantém o café intacto</span>
-          </Benefits>
-          <Benefits>
-            <span><Timer size={20} /></span>
-            <span>Entrega rápida e rastreada</span>
-          </Benefits>
-          <Benefits>
-            <span><Coffee size={20} /></span>
-            <span>O café chega fresquinho até você</span>
-          </Benefits>
-        </BenefitsContainer> 
-      </InformationalSpace>
-      
-      <div> 
-        <img
-          src={bigCoffeeImg}
-          alt="Imagem de um grande copo de café de cor banca com preto e vários complementos vitaminicos em volta."
-        />
-      </div>
-    </PresentationCardContainer>
+    <>
+      <PresentationCardContainer
+        style={{
+          backgroundImage: `url(${backgroundImg})`
+        }}
+      >
+        <InformationalSpace>
+          <FlashyMessageContainer>
+            <FlashyTitle>Encontre o café perfeito para qualquer hora do dia</FlashyTitle>
+            <SubTitle>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</SubTitle>
+          </FlashyMessageContainer>
+        
+          <BenefitsContainer>
+            <Benefits>
+              <span><ShoppingCart size={20} /></span>  
+              <span>Compra simples e segura</span>
+            </Benefits>
+            <Benefits>
+              <span><Package size={20} /></span>
+              <span>Embalagem mantém o café intacto</span>
+            </Benefits>
+            <Benefits>
+              <span><Timer size={20} /></span>
+              <span>Entrega rápida e rastreada</span>
+            </Benefits>
+            <Benefits>
+              <span><Coffee size={20} /></span>
+              <span>O café chega fresquinho até você</span>
+            </Benefits>
+          </BenefitsContainer> 
+        </InformationalSpace>
+        
+        <CoffeeListContainer> 
+          <img
+            src={bigCoffeeImg}
+            alt="Imagem de um grande copo de café de cor banca com preto e vários complementos vitaminicos em volta."
+          />
+        </CoffeeListContainer>
+      </PresentationCardContainer>
+
+      <CoffeeListContainer>
+        <header>
+          <CoffeeListTitle>Nossos cafés</CoffeeListTitle>
+        </header>
+
+        <CoffeeList>
+        <CoffeeCard>
+            <header>
+              <img src='src/assets/coffees/espresso-coffee.svg' alt="Café expresso" />
+            </header>
+
+            <main>
+              <h3>Title</h3>
+              <span>Description</span>
+            </main>
+
+            <footer>
+              <form>
+                <div>
+                  <span>R$</span>
+                  <span>9,90</span>
+                </div>
+                <div>
+                  <button type='button'>-</button>
+                  <span>1</span>
+                  <button type='button'>+</button>
+                </div>
+                <button type='submit'>
+                  <ShoppingCart />
+                </button>
+              </form>
+            </footer>
+          </CoffeeCard>
+
+          <CoffeeCard>
+            <header>
+              <img src='src/assets/coffees/espresso-coffee.svg' alt="Café expresso" />
+            </header>
+
+            <main>
+              <h3>Title</h3>
+              <span>Description</span>
+            </main>
+
+            <footer>
+              <form>
+                <div>
+                  <span>R$</span>
+                  <span>9,90</span>
+                </div>
+                <div>
+                  <button type='button'>-</button>
+                  <span>1</span>
+                  <button type='button'>+</button>
+                </div>
+                <button type='submit'>
+                  <ShoppingCart />
+                </button>
+              </form>
+            </footer>
+          </CoffeeCard>
+
+          <CoffeeCard>
+            <header>
+              <img src='src/assets/coffees/espresso-coffee.svg' alt="Café expresso" />
+            </header>
+
+            <main>
+              <h3>Title</h3>
+              <span>Description</span>
+            </main>
+
+            <footer>
+              <form>
+                <div>
+                  <span>R$</span>
+                  <span>9,90</span>
+                </div>
+                <div>
+                  <button type='button'>-</button>
+                  <span>1</span>
+                  <button type='button'>+</button>
+                </div>
+                <button type='submit'>
+                  <ShoppingCart />
+                </button>
+              </form>
+            </footer>
+          </CoffeeCard>
+
+          <CoffeeCard>
+            <header>
+              <img src='src/assets/coffees/espresso-coffee.svg' alt="Café expresso" />
+            </header>
+
+            <main>
+              <h3>Title</h3>
+              <span>Description</span>
+            </main>
+
+            <footer>
+              <form>
+                <div>
+                  <span>R$</span>
+                  <span>9,90</span>
+                </div>
+                <div>
+                  <button type='button'>-</button>
+                  <span>1</span>
+                  <button type='button'>+</button>
+                </div>
+                <button type='submit'>
+                  <ShoppingCart />
+                </button>
+              </form>
+            </footer>
+          </CoffeeCard>
+        </CoffeeList>
+      </CoffeeListContainer>
+    </>
   );
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -51,6 +51,169 @@ export const Benefits = styled.div`
   color: ${(props) => props.theme["base-text"]};
 `
 
+export const CoffeeListContainer = styled.div`
+  padding: 6rem 10rem;
+`
+
+export const CoffeeListTitle = styled.h1`
+  font-family: 'Baloo 2', sans-serif;
+  font-weight: bold;
+  font-size: 2rem;
+
+  margin-bottom: 1rem;
+
+  color: ${(props) => props.theme["base-subtitle"]};
+`
+
+export const CoffeeList = styled.main`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+
+`
+
+export const CoffeeCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;  
+  text-align: center;
+
+  header {
+    display: flex;
+    flex-direction: column;
+
+    img {
+      margin-bottom: 12px;
+    }
+
+    > div {
+      margin-bottom: 1rem;
+    }
+
+    span {
+      padding: 0.25rem 0.5rem;
+      width: 3.875rem;
+      height: 1.3125rem;
+
+      border-radius: 100px;
+
+      font-size: 0.625rem;
+      font-weight: 700;
+      line-height: 0.8125rem;
+
+      background: ${(props) => props.theme["yellow-light"]};
+      color: ${(props) => props.theme["yellow-dark"]};
+
+      & + span {
+        margin-left: 0.25rem;
+      }
+    }
+  }
+
+  main {
+    margin-bottom: 2.0625rem;
+
+    h3 {
+      font-family: 'Ballo 2', sans-serif;
+      font-size: 1.25rem;
+      font-weight: 700;
+      line-height: 1.625rem;
+      margin-bottom: 0.5rem;
+
+      color: ${(props) => props.theme["base-subtitle"]};
+    }
+    
+    span {
+      font-family: 'Roboto', sans-serif;
+      font-size: 0.875rem;
+      font-weight: 400;
+      line-height: 1.125rem;
+
+      color: ${(props) => props.theme["base-label"]};
+    }
+  }
+
+  form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    div:first-child {
+      > span:first-child {
+        font-family: 'Roboto', sans-serif;
+        font-size: 0.875rem;
+        font-weight: 400;
+        line-height: 1.125rem;
+
+        margin-right: 0.25rem;
+
+        color: ${(props) => props.theme["base-text"]};
+      }
+
+      > span:nth-child(2) {
+        font-family: 'Ballo 2', sans-serif;
+        font-size: 1.25rem;
+        font-weight: 800;
+        line-height: 1.625rem;
+
+        margin-right: 1.4375rem;
+
+        color: ${(props) => props.theme["base-text"]};
+      }
+    }
+
+    div:nth-child(2) {
+      display: flex;
+      align-items: center;
+      justify-content: space-evenly;
+
+      margin-right: 0.5rem;
+
+      font-family: 'Roboto', sans-serif;
+      font-size: 1rem;
+      font-weight: 400;
+      line-height: 1.3125rem;
+      text-align: center;
+
+      border: none;
+
+      padding: 0.5rem;
+      gap: 0.25rem;
+
+      width: 4.5rem;
+      height: 2.375rem;
+
+      background: ${(props) => props.theme["base-button"]};
+      border-radius: 6px;
+    }
+
+    button {
+      border: none;
+      background: none;
+
+      cursor: pointer;
+
+      color: ${(props) => props.theme.purple};
+    }
+
+    > button {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 0.5rem;
+
+      width: 2.375rem;
+      height: 2.375rem;
+
+      background: ${(props) => props.theme["purple-dark"]};
+      color: ${(props) => props.theme["base-card"]};
+
+      border-radius: 6px;
+    }
+  }
+`
+
 
 
 
