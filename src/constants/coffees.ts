9efad4601954bb254a58e3a68334951e8835a328
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2022-08-31 21:48:17 -0300

    fix: valor de café incorreto

--- src/constants/coffees.ts
+++ src/constants/coffees.ts
@@ -10,84 +10,84 @@ export const coffeeList: CoffeeType[] = [
     img: 'src/assets/coffees/espresso-coffee.svg',
     title: 'Expresso Tradicional',
     description: 'O tradicional café feito com água quente e grãos moídos',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/american-coffee.svg',
     title: 'Expresso Americano',
     description: 'Expresso diluído, menos intenso que o tradicional',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/creamy-espresso.svg',
     title: 'Expresso Cremoso',
     description: 'Café expresso tradicional com espuma cremosa',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/iced-coffee.svg',
     title: 'Expresso Gelado',
     description: 'Bebida preparada com café expresso e cubos de gelo',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/coffee-with-milk.svg',
     title: 'Café com Leite',
     description: 'Meio a meio de expresso tradicional com leite vaporizado',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/latte-coffee.svg',
     title: 'Latte',
     description: 'Uma dose de café expresso com o dobro de leite e espuma cremosa',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/capuccino.svg',
     title: 'Capuccino',
     description: 'Bebida com canela feita de doses iguais de café, leite e espuma',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/macchiato-coffee.svg',
     title: 'Macchiato',
     description: 'Café expresso misturado com um pouco de leite quente e espuma',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/mocaccino-coffee.svg',
     title: 'Mocaccino',
     description: 'Café expresso com calda de chocolate, pouco leite e espuma',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/hot-chocolate.svg',
     title: 'Chocolate Quente',
     description: 'Bebida feita com chocolate dissolvido no leite quente e café',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/cuban-coffee.svg',
     title: 'Cubano',
     description: 'Drink gelado de café expresso com rum, creme de leite e hortelã',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/havaiano-coffee.svg',
     title: 'Havaiano',
     description: 'Bebida adocicada preparada com café e leite de coco',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/arabic-coffee.svg',
     title: 'Árabe',
     description: 'Bebida preparada com grãos de café árabe e especiarias',
-    value: 0
+    value: 9.90
   },
   {
     img: 'src/assets/coffees/irish-coffee.svg',
     title: 'Irlandês',
     description: 'Bebida a base de café, uísque irlandês, açúcar e chantilly',
-    value: 0
+    value: 9.90
   }
 ];
